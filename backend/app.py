ef0e253dfdf49235201d255c3425212a33a6cf6d
Felipe Viana <felipe.wilson@aluno.faculdadeimpacta.com.br>
2025-11-17 17:08:31 -0300

    Adicionar suporte para deploy no Render

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from models import db
 from database import DatabaseConfig
 from routes.tasks import app
+import os
 
 app.config.from_object(DatabaseConfig)
 
@@ -10,4 +11,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
